1bd15e3c39529a9706d1b2a480d64842ee2e0c06
Cullen Ruscin <cullen.ruscin@icloud.com>
2023-07-22 17:17:17 -0500

    Add SQL scripts

---
+++ SQL/Postr_Create_DB.sql
@@ -0,0 +1,32 @@
+USE [master]
+
+IF db_id('Postr') IS NULL
+	CREATE DATABASE [Postr]
+GO
+
+USE [Postr];
+GO
+
+DROP TABLE IF EXISTS [User]
+DROP TABLE IF EXISTS [Post]
+
+CREATE TABLE [User] (
+  [Id] int PRIMARY KEY IDENTITY(1, 1),
+  [FirebaseUserId] varchar(28) NOT NULL,
+  [Username] varchar(32) NOT NULL,
+  [Password] varchar(64) NOT NULL,
+  [Email] varchar(64) NOT NULL,
+  [CreateDate] datetime NOT NULL
+)
+GO
+
+CREATE TABLE [Post] (
+  [Id] int PRIMARY KEY IDENTITY(1, 1),
+  [UserId] int NOT NULL,
+  [Content] varchar(256) NOT NULL,
+  [CreateDate] datetime NOT NULL
+)
+GO
+
+ALTER TABLE [Post] ADD FOREIGN KEY ([UserId]) REFERENCES [User] ([Id])
+GO
---
+++ SQL/Postr_Seed_Data.sql
@@ -0,0 +1,20 @@
+﻿USE [Postr]
+GO
+
+SET IDENTITY_INSERT [User] ON
+INSERT INTO [User] ([Id], [FirebaseUserId], [Username], [Password], [Email], [CreateDate])
+VALUES (1, 'EpQ5n0ySTHZQNFQ6YPhAKbVzPDz1', 'gebohix', 'password', 'gebohix172@rc3s.com', GETDATE()),
+	   (2, 'tlyVavudNmhyws5J4k0iTde3Nat1', 'velixeg', 'password', 'velixeg772@rc3s.com', GETDATE()),
+	   (3, 'DSc4ZJIdovY1ctCUdUOV5QflyNH3', 'hegove', 'password', 'hegove7174@rc3s.com', GETDATE())
+SET IDENTITY_INSERT [User] OFF
+
+SET IDENTITY_INSERT [Post] ON
+INSERT INTO [Post] ([Id], [UserId], [Content], [CreateDate])
+VALUES (1, 1, 'Hello World!', GETDATE()),
+       (2, 2, 'This is my first post!', GETDATE()),
+	   (3, 3, 'Nice to see you all!', GETDATE())
+SET IDENTITY_INSERT [Post] OFF
+
+
+
+
